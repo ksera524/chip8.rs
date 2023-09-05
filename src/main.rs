12567aff7608daace8a0c15fc5f73dda42a70296
ksera524 <ksera631@gmail.com>
2023-09-05 12:54:17 +0900

    デバッグ

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+const DISPLAY_WIDTH: usize = 64;
+const DISPLAY_HEIGHT: usize = 32;
+
 struct CPU {
     registers: [u8; 16],
-    position_in_memory: usize,//program counter
+    position_in_memory: usize, //program counter
     memory: [u8; 0x1000],
     stack: [u16; 16],
     stack_pointer: usize,
-    index_register: u16, 
+    index_register: u16,
     delay_timer: u8,
+    display: [[bool; DISPLAY_WIDTH]; DISPLAY_HEIGHT],
 }
 
 impl CPU {
@@ -23,11 +27,13 @@ impl CPU {
             stack_pointer: 0,
             index_register: 0,
             delay_timer: 0,
+            display: [[false; DISPLAY_WIDTH]; DISPLAY_HEIGHT],
         };
 
         let mut file = File::open(Path::new(file_path)).expect("Failed to open the file");
         let mut buffer: Vec<u8> = Vec::new();
-        file.read_to_end(&mut buffer).expect("Failed to read the file");
+        file.read_to_end(&mut buffer)
+            .expect("Failed to read the file");
 
         for (i, byte) in buffer.iter().enumerate() {
             cpu.memory[0x200 + i] = *byte;
@@ -56,49 +62,170 @@ impl CPU {
             let nnn = opcode & 0x0FFF;
             let kk = opcode & 0x00FF;
 
+            println!(
+                "opcode: {:04x} c: {:01x} x: {:01x} y: {:01x} d: {:01x} nnn: {:03x} kk: {:02x}",
+                opcode, c, x, y, d, nnn, kk
+            );
+
             match (c, x, y, d) {
-                (0, 0, 0, 0) => {return;}
-                (0, _, _, _) => self.sus_addr(nnn),
-                (0, 0, 0xE, 0xE) => self.ret(),
-                (0x1, _, _, _) => self.jp_addr(nnn),
-                (0x2, _, _, _) => self.call(nnn),
-                (0x3, _, _, _) => self.se_byte(x, kk as u8),
-                (0x4, _, _, _) => self.sne_byte(x, kk as u8),
-                (0x5, _, _, 0) => self.se_xy(x,y),
-                (0x6, _, _, _) => self.ld_byte(x,kk as u8),
-                (0x7, _, _, _) => self.add_byte(x,kk as u8),
-                (0x8, _, _, 0) => self.ld_xy(x,y),
-                (0x8, _, _, 1) => self.or_xy(x,y),
-                (0x8, _, _, 2) => self.and_xy(x,y),
-                (0x8, _, _, 3) => self.xor_xy(x,y),
-                (0x8, _, _, 4) => self.add_xy(x,y),
-                (0x8, _, _, 5) => self.sub_xy(x,y),
-                (0x8, _, _, 6) => self.shr_xy(x),
-                (0x8, _, _, 7) => self.subn_xy(x,y),
-                (0x8, _, _, 0xE) => self.shl_xy(x),
-                (0x9, _, _, 0) => self.sne_xy(x,y),
-                (0xA, _, _, _) => self.ld_i_addr(nnn),
-                (0xB, _, _, _) => self.jp_v0_addr(nnn),
-                (0xC, _, _, _) => self.rnd_byte(x,kk as u8),
-                (0xD, _, _, _) => self.drw_xy(x,y,d),
-                (0xE, _, 9, 0xE) => self.skp_vx(x),
-                (0xE, _, 0xA, 1) => self.sknp_vx(x),
-                (0xF, _, 0, 7) => self.ld_vx_dt(x),
-                (0xF, _, 0, 0xA) => self.ld_vx_k(x),
-                (0xF, _, 1, 5) => self.ld_dt_vx(x),
-                (0xF, _, 1, 8) => self.ld_st_vx(x),
-                (0xF, _, 1, 0xE) => self.add_i_vx(x),
-                (0xF, _, 2, 9) => self.ld_f_vx(x),
-                (0xF, _, 3, 3) => self.ld_b_vx(x),
-                (0xF, _, 5, 5) => self.ld_i_vx(x),
-                (0xF, _, 6, 5) => self.ld_vx_i(x),
-                _ => todo!("opcode {:04x}", opcode),
+                (0, 0, 0, 0) => {
+                    println!("Executing function: return");
+                    return;
+                }
+                (0, _, _, _) => {
+                    println!("Executing function: sys_addr");
+                    self.sys_addr(nnn);
+                }
+                (0, 0, 0xE, 0) => {
+                    println!("Executing function: cls");
+                    self.cls();
+                }
+                (0, 0, 0xE, 0xE) => {
+                    println!("Executing function: ret");
+                    self.ret();
+                }
+                (0x1, _, _, _) => {
+                    println!("Executing function: jp_addr");
+                    self.jp_addr(nnn);
+                }
+                (0x2, _, _, _) => {
+                    println!("Executing function: call");
+                    self.call(nnn);
+                }
+                (0x3, _, _, _) => {
+                    println!("Executing function: se_byte");
+                    self.se_byte(x, kk as u8);
+                }
+                (0x4, _, _, _) => {
+                    println!("Executing function: sne_byte");
+                    self.sne_byte(x, kk as u8);
+                }
+                (0x5, _, _, 0) => {
+                    println!("Executing function: se_xy");
+                    self.se_xy(x, y);
+                }
+                (0x6, _, _, _) => {
+                    println!("Executing function: ld_byte");
+                    self.ld_byte(x, kk as u8);
+                }
+                (0x7, _, _, _) => {
+                    println!("Executing function: add_byte");
+                    self.add_byte(x, kk as u8);
+                }
+                (0x8, _, _, 0) => {
+                    println!("Executing function: ld_xy");
+                    self.ld_xy(x, y);
+                }
+                (0x8, _, _, 1) => {
+                    println!("Executing function: or_xy");
+                    self.or_xy(x, y);
+                }
+                (0x8, _, _, 2) => {
+                    println!("Executing function: and_xy");
+                    self.and_xy(x, y);
+                }
+                (0x8, _, _, 3) => {
+                    println!("Executing function: xor_xy");
+                    self.xor_xy(x, y);
+                }
+                (0x8, _, _, 4) => {
+                    println!("Executing function: add_xy");
+                    self.add_xy(x, y);
+                }
+                (0x8, _, _, 5) => {
+                    println!("Executing function: sub_xy");
+                    self.sub_xy(x, y);
+                }
+                (0x8, _, _, 6) => {
+                    println!("Executing function: shr_xy");
+                    self.shr_xy(x);
+                }
+                (0x8, _, _, 7) => {
+                    println!("Executing function: subn_xy");
+                    self.subn_xy(x, y);
+                }
+                (0x8, _, _, 0xE) => {
+                    println!("Executing function: shl_xy");
+                    self.shl_xy(x);
+                }
+                (0x9, _, _, 0) => {
+                    println!("Executing function: sne_xy");
+                    self.sne_xy(x, y);
+                }
+                (0xA, _, _, _) => {
+                    println!("Executing function: ld_i_addr");
+                    self.ld_i_addr(nnn);
+                }
+                (0xB, _, _, _) => {
+                    println!("Executing function: jp_v0_addr");
+                    self.jp_v0_addr(nnn);
+                }
+                (0xC, _, _, _) => {
+                    println!("Executing function: rnd_byte");
+                    self.rnd_byte(x, kk as u8);
+                }
+                (0xD, _, _, _) => {
+                    println!("Executing function: drw_xy");
+                    self.drw_xy(x, y, d);
+                }
+                (0xE, _, 9, 0xE) => {
+                    println!("Executing function: skp_vx");
+                    self.skp_vx(x);
+                }
+                (0xE, _, 0xA, 1) => {
+                    println!("Executing function: sknp_vx");
+                    self.sknp_vx(x);
+                }
+                (0xF, _, 0, 7) => {
+                    println!("Executing function: ld_vx_dt");
+                    self.ld_vx_dt(x);
+                }
+                (0xF, _, 0, 0xA) => {
+                    println!("Executing function: ld_vx_k");
+                    self.ld_vx_k(x);
+                }
+                (0xF, _, 1, 5) => {
+                    println!("Executing function: ld_dt_vx");
+                    self.ld_dt_vx(x);
+                }
+                (0xF, _, 1, 8) => {
+                    println!("Executing function: ld_st_vx");
+                    self.ld_st_vx(x);
+                }
+                (0xF, _, 1, 0xE) => {
+                    println!("Executing function: add_i_vx");
+                    self.add_i_vx(x);
+                }
+                (0xF, _, 2, 9) => {
+                    println!("Executing function: ld_f_vx");
+                    self.ld_f_vx(x);
+                }
+                (0xF, _, 3, 3) => {
+                    println!("Executing function: ld_b_vx");
+                    self.ld_b_vx(x);
+                }
+                (0xF, _, 5, 5) => {
+                    println!("Executing function: ld_i_vx");
+                    self.ld_i_vx(x);
+                }
+                (0xF, _, 6, 5) => {
+                    println!("Executing function: ld_vx_i");
+                    self.ld_vx_i(x);
+                }
+                _ => {
+                    println!("Executing unknown opcode");
+                    todo!("opcode {:04x}", opcode);
+                }
             }
         }
     }
 
-    fn sus_addr(&mut self, addr: u16) {
-        todo!();
+    fn sys_addr(&mut self, addr: u16) {
+        self.position_in_memory = addr as usize;
+    }
+
+    fn cls(&mut self) {
+        self.display = [[false; DISPLAY_WIDTH]; DISPLAY_HEIGHT];
     }
 
     fn ret(&mut self) {
@@ -191,7 +318,7 @@ impl CPU {
         }
     }
 
-    fn sub_xy(&mut self,x:u8,y:u8){
+    fn sub_xy(&mut self, x: u8, y: u8) {
         let args1 = self.registers[x as usize];
         let args2 = self.registers[y as usize];
 
@@ -264,7 +391,24 @@ impl CPU {
     }
 
     fn drw_xy(&mut self, x: u8, y: u8, n: u8) {
-        todo!("Implement this")
+        let x = self.registers[x as usize] as usize;
+        let y = self.registers[y as usize] as usize;
+
+        self.registers[0xF] = 0;
+
+        for byte in 0..n {
+            let byte = self.memory[self.index_register as usize + byte as usize];
+            for bit in 0..8 {
+                let bit = (byte >> (7 - bit)) & 1;
+                let x = (x + bit as usize) % DISPLAY_WIDTH;
+                let y = (y + byte as usize) % DISPLAY_HEIGHT;
+                let prev = self.display[y][x];
+                self.display[y][x] ^= bit == 1;
+                if prev && !self.display[y][x] {
+                    self.registers[0xF] = 1;
+                }
+            }
+        }
     }
 
     fn skp_vx(&mut self, x: u8) {
