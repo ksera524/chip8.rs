71ba5326a47800e9a6cf39acfc5624b381b1e344
ksera524 <ksera631@gmail.com>
2024-07-06 01:10:19 +0900

    chore: Update README.md with BRIX game controls in Japanese

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
 use std::thread;
 
 const DISPLAY_WIDTH: usize = 64;
@@ -40,24 +39,12 @@ struct Cpu {
     index_register: u16,
     display: [[bool; DISPLAY_WIDTH]; DISPLAY_HEIGHT],
     sound_timer: u8,
-    delay_timer: Arc<Mutex<u8>>,
-    delay_timer_thread: Option<thread::JoinHandle<()>>,
+    delay_timer:u8,
     key: Option<u8>,
 }
 
 impl Cpu {
     fn new(file_path: &str) -> Cpu {
-        let delay_timer = Arc::new(Mutex::new(0));
-        let delay_timer_clone = Arc::clone(&delay_timer);
-
-        let delay_timer_thread = Some(thread::spawn(move || loop {
-            let mut delay_timer = delay_timer_clone.lock().unwrap();
-            if *delay_timer > 0 {
-                *delay_timer -= 1;
-            }
-
-            thread::sleep(std::time::Duration::from_millis(1000 / 60));
-        }));
 
         let mut cpu = Cpu {
             registers: [0; 16],
@@ -66,8 +53,7 @@ impl Cpu {
             stack: [0; 16],
             stack_pointer: 0,
             index_register: 0,
-            delay_timer,
-            delay_timer_thread,
+            delay_timer: 0,
             sound_timer: 0,
             display: [[false; DISPLAY_WIDTH]; DISPLAY_HEIGHT],
             key: None,
@@ -112,7 +98,7 @@ impl Cpu {
 
         for row in &self.display {
             for &pixel in row {
-                buffer.push(if pixel { '█' } else { ' ' });
+                buffer.push(if pixel { '#' } else { ' ' });
             }
             buffer.push('\n');
         }
@@ -125,8 +111,17 @@ impl Cpu {
         std::io::stdout().flush().unwrap();
     }
 
+    fn decrement_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        }
+    }
+
     fn update(&mut self, keyboard_receiver: &mut mpsc::Receiver<u8>) {
-        let dt = *self.delay_timer.lock().unwrap();
         info!(
             "v={:?} i={}({:x}) stack={:?} sp={:x} pc={}({:x}) dt={:x} key={:?}",
             self.registers,
@@ -136,7 +131,7 @@ impl Cpu {
             self.stack_pointer,
             self.position_in_memory,
             self.position_in_memory,
-            dt,
+            self.delay_timer,
             self.key
         );
 
@@ -490,9 +485,8 @@ impl Cpu {
 
     fn ld_vx_dt(&mut self, x: u8) {
         let vx = self.registers[x as usize];
-        let delay_timer = self.delay_timer.lock().unwrap();
         info!("Executing function: ld_vx_dt x: {} vx: {}", x, vx);
-        self.registers[x as usize] = *delay_timer
+        self.registers[x as usize] = self.delay_timer;
     }
 
     fn ld_vx_k(&mut self, x: u8, keyboard_receiver: &mut mpsc::Receiver<u8>) {
@@ -514,8 +508,8 @@ impl Cpu {
             "Executing function: ld_dt_vx x: {} vx: {}",
             x, self.registers[x as usize]
         );
-        let mut delay_timer = self.delay_timer.lock().unwrap();
-        *delay_timer = self.registers[x as usize];
+       
+        self.delay_timer = self.registers[x as usize];
     }
 
     fn ld_st_vx(&mut self, x: u8) {
@@ -586,16 +580,12 @@ impl Cpu {
         });
 
         loop {
+            for _ in 0..10 {
             self.update(&mut keyboard_receiver);
+            }
+            self.decrement_timers();
             self.draw();
-        }
-    }
-}
-
-impl Drop for Cpu {
-    fn drop(&mut self) {
-        if let Some(handle) = self.delay_timer_thread.take() {
-            handle.join().unwrap();
+            
         }
     }
 }
@@ -617,7 +607,7 @@ fn main() {
     .unwrap();
 
     info!("Starting the emulator");
-    let mut cpu = Cpu::new("rom/Tetris [Fran Dachille, 1991].ch8");
+    let mut cpu = Cpu::new("rom/BRIX");
 
     cpu.start();
 }
